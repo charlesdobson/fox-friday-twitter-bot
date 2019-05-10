df30e5bdd172445191e4b4709d61a983fe62398c
charlesdobson <charlesdobson92@gmail.com>
2019-05-10 11:20:37 -0400

    Updated path for fox images

--- foxfriday.py
+++ foxfriday.py
@@ -14,7 +14,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Change to images folder
-os.chdir('images/foxes')
+os.chdir('/Users/charlie/documents/projects/fox-friday-twitter-bot/images/foxes')
 
 # Loop through images
 for image in os.listdir('.'):
